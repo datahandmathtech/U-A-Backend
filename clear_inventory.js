9a9ad7bc7c26e8781514d9aa9cf04088f1f38b41
Abhaypraja09 <abhaypraja09@gmail.com>
2026-08-24 17:09:28 +0530

    chore: push frontend changes for fix inventory quantity showing 0

---
+++ clear_inventory.js
@@ -0,0 +1,25 @@
+﻿const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function main() {
+  console.log("Deleting InventoryLog...");
+  await prisma.inventoryLog.deleteMany({});
+  
+  console.log("Deleting Inventory...");
+  await prisma.inventory.deleteMany({});
+  
+  // Also clear DailyStockBalance just in case
+  console.log("Deleting DailyStockBalance...");
+  await prisma.dailyStockBalance.deleteMany({});
+
+  console.log("All inventory data deleted successfully.");
+}
+
+main()
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
